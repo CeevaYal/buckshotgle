9d5bc2f5fef595004e79c5ad00bc8049bc457345
CeevaYal <ceevayal@gmail.com>
2026-06-01 11:31:02 -0700

    first commit

---
+++ buckshot_bg.wasm.d.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_get_interface_engaging: (a: number) => number;
+export const __wbg_interface_free: (a: number, b: number) => void;
+export const __wbg_set_interface_engaging: (a: number, b: number) => void;
+export const interface_new: () => number;
+export const interface_post: (a: number, b: number, c: number) => [number, number];
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
